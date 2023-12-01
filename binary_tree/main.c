f445b007da8da18ab5552c31c443ccc2a18903b6
Felipe Barichello <87396379+felipebarichello@users.noreply.github.com>
2023-11-30 23:52:56 -0300

    BST: Main print

--- binary_tree/main.c
+++ binary_tree/main.c
@@ -5,6 +5,8 @@
 int main() {
     BinarySearchTree tree = BSTCreate();
 
+    BSTPrintList(tree, BST_TRAVERSAL_IN, BST_LEFT);
+
     BSTInsertOrdered(&tree, 5); //       5        |
     BSTInsertOrdered(&tree, 3); //     /   \      |
     BSTInsertOrdered(&tree, 7); //    3     7     |
@@ -12,6 +14,8 @@ int main() {
     BSTInsertOrdered(&tree, 4); //  2   4 6   8   |
     BSTInsertOrdered(&tree, 6); //                |
     BSTInsertOrdered(&tree, 8); //                |
+
+    BSTPrintList(tree, BST_TRAVERSAL_IN, BST_LEFT);
 
     printf("O programa roda\n");
 
